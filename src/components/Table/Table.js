8b45998ed1a47534266affcb8eda099b0d56fe5e
toia-19 <vickingiediaz19@gmail.com>
2024-11-21 17:10:22 -0300

    Arreglos en diseño de la tabla; cambio en íconos

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -3,7 +3,7 @@ import './Table.css';
 // REACT
 import React from 'react';
 
-// MUI
+// MUI - TABLA
 import Table from '@mui/material/Table';
 
 import TableBody from '@mui/material/TableBody';
@@ -18,18 +18,31 @@ import TableRow from '@mui/material/TableRow';
 
 import Paper from '@mui/material/Paper';
 
-function createData(title, subTitle, image, content, price) {
-    return { title, subTitle, image, content, price };
+// BOTONES
+import Tooltip from '@mui/material/Tooltip';
+
+import IconButton from '@mui/material/IconButton';
+
+import DeleteIcon from '@mui/icons-material/Delete';
+
+import EditIcon from '@mui/icons-material/Edit';
+
+// COLORES
+import { yellow } from '@mui/material/colors';
+
+import { red } from '@mui/material/colors';
+
+function createData(title, subTitle, image, content, price, actions) {
+    return { title, subTitle, image, content, price, actions };
 }
 
 function CustomTable() {
-
     const rows = [
-        createData('Xilófono', 'Instrumento de aire', <img src='' alt='Adaptador'/>, '', 28000),
-        createData('Redoblante', 'Instrumento de percusión', <img src='' alt='Adaptador'/>, '', 38000),
-        createData('Violín', 'Instrumento de cuerda', <img src='' alt='Adaptador'/>, '', 70000),
-        createData('Guitarra', 'Instrumento de cuerda', <img src='' alt='Adaptador'/>, '', 65000),
-        createData('Adaptador', 'Midi', <img src='../../Images/CableMIDIestándar_PuertoJoystick.png' alt='Adaptador'/>, 'Cable MIDI estándar. Puerto Joystick.', 10000),
+        createData('Xilófono', 'Instrumento de aire', <img src='https://d1aeri3ty3izns.cloudfront.net/media/10/102273/1200/preview.jpg' alt='Xilófono' class='img-table' />, 'Instrumento de percusión idiófono formado por barras de madera que emiten notas afinadas.', 28000),
+        createData('Redoblante', 'Instrumento de percusión', <img src='https://definicion.de/wp-content/uploads/2014/01/redoblante.jpg' alt='Redoblante' class='img-table' />, 'Tambor con bordón que se golpea con una baqueta. Se usa en bandas militares y orquestas, y su sonido depende de la tensión de los parches y el material de la bordonera.', 38000),
+        createData('Violín', 'Instrumento de cuerda', <img src='https://th.bing.com/th/id/R.07eac50dd4bef48266f0f0ec2ae95011?rik=PS9AH15KSXV40Q&riu=http%3a%2f%2fupload.wikimedia.org%2fwikipedia%2fcommons%2ff%2ff4%2fGerman%2c_maple_Violin.JPG&ehk=%2fkZJpKmWHUsjz370ACgGeDTaq8UMDDaaG5VeVXbY3PY%3d&risl=&pid=ImgRaw&r=0' alt='Violín' class='img-table' />, 'instrumento musical de cuerda y arco originario de Italia', 70000),
+        createData('Guitarra', 'Instrumento de cuerda', <img src='https://th.bing.com/th/id/OIP.krc0dZ0OFzKJYnN4CqOn3QHaHa?rs=1&pid=ImgDetMain' alt='Guitarra' class='img-table' />, 'Instrumento de cuerda pulsada, compuesto de una caja de resonancia, un mástil sobre el que va adosado el diapasón o trastero', 65000),
+        createData('Adaptador', 'Midi', <img src='https://thumbs4.static-thomann.de/thumb/thumb300x/pics/cms/image/guide/en/midi_interfaces/0027_soundblasterkabel.jpg' alt='Adaptador' class='img-table' />, 'Cable MIDI estándar. Puerto Joystick.', 10000),
     ];
 
     return (
@@ -38,11 +51,12 @@ function CustomTable() {
                 <Table sx={{ minWidth: 650 }} aria-label="simple table">
                     <TableHead>
                         <TableRow>
-                            <TableCell>Título</TableCell>
-                            <TableCell align="right">Subtítulo</TableCell>
-                            <TableCell align="right">Imagen</TableCell>
-                            <TableCell align="right">Descripción</TableCell>
-                            <TableCell align="right">Precio</TableCell>
+                            <TableCell class="row" align="center">Título</TableCell>
+                            <TableCell class="row" align="center">Subtítulo</TableCell>
+                            <TableCell class="row" align="center">Imagen</TableCell>
+                            <TableCell class="row" align="center">Descripción</TableCell>
+                            <TableCell class="row" align="center">Precio</TableCell>
+                            <TableCell class="row" align="center">Acciones</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
@@ -51,11 +65,25 @@ function CustomTable() {
                                 key={row.title}
                                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                             >
-                                <TableCell component="th" scope="row">{row.title}</TableCell>
-                                <TableCell align="right">{row.subTitle}</TableCell>
-                                <TableCell align="right">{row.image}</TableCell>
-                                <TableCell align="right">{row.content}</TableCell>
-                                <TableCell align="right">{row.price}</TableCell>
+                                <TableCell class="row" component="th" scope="row">{row.title}</TableCell>
+                                <TableCell class="row" align="center">{row.subTitle}</TableCell>
+                                <TableCell class="row" align="center">{row.image}</TableCell>
+                                <TableCell class="row" align="center">{row.content}</TableCell>
+                                <TableCell class="row" align="center">${row.price}</TableCell>
+
+                                <TableCell class="row" align="center">
+                                    <Tooltip title="Editar" class="btn-edit">
+                                        <IconButton>
+                                            <EditIcon sx={{ color: yellow[500] }} />
+                                        </IconButton>
+                                    </Tooltip>
+
+                                    <Tooltip title="Eliminar" class="btn-delete">
+                                        <IconButton>
+                                            <DeleteIcon sx={{ color: red[600] }} />
+                                        </IconButton>
+                                    </Tooltip>
+                                </TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
